373c23b2c5d4737f500bcb6b6e3fdc66498d0145
h-sumiya <sumiya.main01@gmail.com>
2023-08-16 22:38:17 +0900

    init

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,59 @@ fn main() {
     let mut v2 = Vec::new();
     load(&mut buf.as_slice(), &mut v2).unwrap();
     println!("{:?}", v2);
+
+    let v1 = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let v2 = vec![1, 3, 5, 7, 9, 11, 13, 15, 17];
+    let v3 = vec![1, 4, 7, 10, 13, 16, 19, 22, 25];
+    let res = and_beta(&[&v1, &v2, &v3]);
+    println!("{:?}", res);
+}
+
+enum Data<'a> {
+    Data(&'a [u32]),
+    Not(&'a [u32]),
+    None,
+    ALL,
 }
 
 fn tag(category: &str, tag: &str) -> Option<Vec<u32>> {
     None
+}
+
+pub fn and_beta(data: &[&[u32]]) -> Vec<u32> {
+    let mut result = Vec::new();
+    let mut indexes = data.iter().map(|_| 0).collect::<Vec<usize>>();
+    let lens = data.iter().map(|d| d.len()).collect::<Vec<usize>>();
+
+    while indexes.iter().zip(lens.iter()).all(|(i, l)| i < l) {
+        let max = indexes
+            .iter()
+            .zip(data.iter())
+            .map(|(i, d)| d[*i])
+            .max()
+            .unwrap();
+        let mut flag = true;
+        'a: for (i, d) in indexes.iter_mut().zip(data.iter()) {
+            while d[*i] < max {
+                flag = false;
+                *i += 1;
+                if *i >= d.len() {
+                    if d[*i - 1] == max {
+                        continue 'a;
+                    } else {
+                        return result;
+                    }
+                }
+            }
+        }
+        if flag {
+            result.push(max);
+            for i in indexes.iter_mut() {
+                *i += 1;
+            }
+        }
+    }
+    result
 }
 
 fn word(word: &str) -> Option<Vec<u32>> {
